16ef851158fa0965f4e340760722121fff347611
OĞUZHAN <oguzn.sari@gmail.com>
2023-01-18 23:31:33 +0300

    5

--- index.php
+++ index.php
@@ -5,12 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
+    <?php
+        $name = "Dark Matter";
+        $read = true;
+
+        if ($read) {
+            $message = "You've read the $name.";
+        } else {
+            $message = "You've not yet read the $name.";
+        }
+    ?>
     <h1>
-        <?php
-            echo "Hello World!";
-        ?>
+        <?= $message; ?>
     </h1>
 </body>
 </html>
